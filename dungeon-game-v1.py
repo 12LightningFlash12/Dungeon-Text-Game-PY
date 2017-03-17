fe6ae065801d98e8fdef017bfa81cbe1a8f0b8c2
LightningFlash <12lightningflash12@gmail.com>
2017-03-16 21:21:19 -0400

    Create dungeon-game-v1.py

---
+++ dungeon-game-v1.py
@@ -0,0 +1,28 @@
+import random
+
+uH = 10
+eH = 50
+slaying = True
+
+while slaying:
+    atk = raw_input("Attack Y or N? ")
+    damage = random.randint(10, 20)
+    eDamage = random.randint(0, 5)
+    
+    if atk == "y" or atk == "Y":
+        print "You attack with %s damage! Also, you take %d damage from the enemy" % (damage, eDamage)
+        eH = eH - damage
+        uH = uH - eDamage
+    if atk == "n" or atk == "N":
+        print "You stand your groung and take %s damage from the enemy" % (eDamage)
+        uH = uH - eDamage
+    print "User Health: %s" % (uH)
+    print "Enemy Health : %s" % (eH)
+    
+    if uH <= 0:
+        slaying = False
+        print "You fall in battle!"
+    
+    if eH <= 0:
+        slaying = False
+        print "YOu hath slain the beast!"
