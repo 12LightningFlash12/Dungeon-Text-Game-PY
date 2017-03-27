8dd26fe01b65d97e22aa78e01fd8a836693af912
LightningFlash <12lightningflash12@gmail.com>
2017-03-27 09:27:17 -0400

    Update dungeon-shop-test.py

--- dungeon-shop-test.py
+++ dungeon-shop-test.py
@@ -1,1 +1,44 @@
+gems = 1000
+shop = True
+
+while shop:
+    print '|----------------------|'
+    print '|     Dungeon Shop     |'
+    print '|----------------------|'
+    print '| 1. Potion  2. Ration |'
+    print '|  50 gems   100 gems  |'
+    print '|----------------------|'
+    
+    print ''
+    
+    item = raw_input('Pick an item to buy: ')
+    print ''
+    
+    if item == '1':
+        if gems >= 50:
+            print 'You recieved 1 potion.'
+            
+            gems = gems - 50
+        else:
+            print 'You do not have enough gems.'
+    elif item == '2':
+        if gems >= 100:
+            print 'You recieved 1 Ration.'
+            
+            gems = gems - 100
+        else:
+            print 'You do not have enough gems.'
+    
+    gems = gems
+    
+    print 'Gems: {}'.format(gems)
+    print ''
+    
+    end = raw_input('End shop (y or n)? ')
+    print ''
+    
+    if end == 'y':
+        shop = False
+    elif end == 'n':
+        shop = True
 
