3ff462e689ad2180f7dc454cb8726205298880e0
LightningFlash <12lightningflash12@gmail.com>
2017-03-28 16:30:14 -0400

    Update dungeon-shop-test.py

--- dungeon-shop-test.py
+++ dungeon-shop-test.py
@@ -1,6 +1,11 @@
 gems = 1000
 shop = True
 
+p = 0
+r = 0
+lp = 0
+lr = 0
+
 while shop:
     print '|---------------------------------------|'
     print '|             Dungeon  Shop             |'
@@ -21,29 +26,41 @@ while shop:
         if gems >= 50:
             print 'You recieved 1 Potion.'
             gems = gems - 50
+            p = p + 1
         else:
             print 'You do not have enough gems.'
     elif item == '2':
         if gems >= 50:
             print 'You recieved 1 Ration.'
             gems = gems - 50
+            r = r + 1
         else:
             print 'You do not have enough gems.'
     elif item == '3':
         if gems >= 100:
             print 'You recieved 1 Large Potion.'
             gems = gems - 100
+            lp = lp + 1
         else:
             print 'You do not have enough gems.'
     elif item == '4':
         if gems >= 100:
             print 'YOu recieved 1 Large Ration.'
             gems = gems - 100
+            lr = lr + 1
         else:
             print 'You do not have enough gems.'
     
     gems = gems
+    p = p
+    r = r
+    lp = lp
+    lr = lr
     
+    print 'Potions: {}'.format(p)
+    print 'Rations: {}'.format(r)
+    print 'Large Potions: {}'.format(lp)
+    print 'Large Rations: {}'.format(lr)
     print 'Gems: {}'.format(gems)
     print ''
     
@@ -55,3 +72,9 @@ while shop:
     elif end == 'n':
         shop = True
 
+print 'Potions: {}'.format(p)
+print 'Rations: {}'.format(r)
+print 'Large Potions: {}'.format(lp)
+print 'Large Rations: {}'.format(lr)
+print 'Gems: {}'.format(gems)
+print ''
